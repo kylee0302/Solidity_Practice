0080496a0e1021370efb216fa856b3d2bbcf28f3
kylee0302 <kylee0293@gmail.com>
2025-07-04 16:48:37 +0900

    Add files via upload
    helloworld.sol

---
+++ helloworld.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier:GPL-3.0
+pragma solidity >=0.7.0 < 0.9.0;
+
+contract Hello{ // 스마트 컨트랙트 명시
+    string public hi = "Hello Solidity"; // hi라는 public 함수에 hello solidity를 넣음
+}
